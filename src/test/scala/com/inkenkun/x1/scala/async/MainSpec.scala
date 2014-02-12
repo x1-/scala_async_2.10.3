ea54afdd1f4f3584f8c3c7a05231b775a0439a57
x1- <viva008@gmail.com>
2014-02-12 17:17:20 +0900

    Mainクラスのテストケースの側だけ書きました。

--- src/test/scala/com/inkenkun/x1/scala/async/MainSpec.scala
+++ src/test/scala/com/inkenkun/x1/scala/async/MainSpec.scala
@@ -1,5 +1,13 @@
 package com.inkenkun.x1.scala.async
 
-class MainSpec {
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.mock.Mockito
 
+class MainSpec extends SpecificationWithJUnit with Mockito {
+
+  "Main.standard1" should {
+    "test case 1:" in {
+      true
+    }
+  }
 }
